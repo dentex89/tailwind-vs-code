a077a7b035889c4dcd6065cd16408e76c78dbf6f
dentex89 <ponchoelizondo@hotmail.com>
2025-10-18 12:55:21 -0600

    gh pages test

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
 import "./style.css";
+
+/* Mobile Menu Toggle */
+
+const menuButton = document.getElementById("menu-button");
+const mobileMenu = document.getElementById("mobile-menu");
+
+menuButton.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
